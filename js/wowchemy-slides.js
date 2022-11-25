23bb11e1e319748e4887d26fca71f8ad759db054
Dmitry S. Kulyabov <yamadharma@gmail.com>
2022-11-25 15:35:06 +0300

    rebuilding site Пт 25 ноя 2022 15:35:06 MSK

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -3,7 +3,7 @@
   var slides = null;
 
   // <stdin>
-  var enabledPlugins = [RevealMarkdown, RevealSearch, RevealNotes, RevealMath.MathJax3, RevealZoom];
+  var enabledPlugins = [RevealMarkdown, RevealSearch, RevealNotes, RevealMath.KaTeX, RevealZoom];
   var isObject = function(o) {
     return o === Object(o) && !isArray(o) && typeof o !== "function";
   };
